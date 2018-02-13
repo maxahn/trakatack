31001aa6f397227a7c61ad605c6694ba0636c2fe
maxahn <ahnmaximus@gmail.com>
2018-02-13 02:35:39 -0800

    altered error message in user model for daily hours and added basic validation testing for creating a new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :work_periods, through: :tasks
   validates :first_name, presence: true
-  validates :daily_hours, numericality: {only_integer: true, greater_than: 0, less_than: 24, message: "Available daily work hours need to be greater than or equal to 1 and less than or equal to 24" }
+  validates :daily_hours, numericality: {only_integer: true, greater_than: 0, less_than: 24, message: "Available daily work hours need to be greater than 0 and less than or equal to 24" }
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
--- spec/features/creating_users_spec.rb
+++ spec/features/creating_users_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "People can create new users" do 
-  scenario "with valid attributes" do 
+  before do
     visit "/"
     click_link "New User"
+  end
+  scenario "with valid attributes" do 
     fill_in "First Name", with: "Amy"
     fill_in "Email", with: "amy@amazingamy.com"
     fill_in "Password", with: "iamnotreallyamy"
@@ -11,6 +13,58 @@ RSpec.feature "People can create new users" do
     fill_in "Daily hours", with: 5 
     click_button "Create User"
     expect(page).to have_content "User was successfully created." 
+  end
+
+  scenario "with invalid email" do
+    fill_in "First Name", with: "Amy"
+    fill_in "Email", with: "amyamazingamy.com"
+    fill_in "Password", with: "iamnotreallyamy"
+    fill_in "Password Confirmation", with: "iamnotreallyamy"
+    fill_in "Daily hours", with: 5 
+    click_button "Create User"
+    expect(page).to have_content "prohibited this user from being saved"
+    expect(page).to have_content "Email is invalid" 
+  end
+
+  scenario "with no name" do
+    fill_in "Email", with: "amyamazingamy.com"
+    fill_in "Password", with: "iamnotreallyamy"
+    fill_in "Password Confirmation", with: "iamnotreallyamy"
+    fill_in "Daily hours", with: 5 
+    click_button "Create User"
+    expect(page).to have_content "prohibited this user from being saved"
+    expect(page).to have_content "First name can't be blank"
+  end
+
+  scenario "with no password" do
+    fill_in "First Name", with: "Amy"
+    fill_in "Email", with: "amyamazingamy.com"
+    fill_in "Daily hours", with: 5 
+    click_button "Create User"
+    expect(page).to have_content "prohibited this user from being saved"
+    expect(page).to have_content "Password can't be blank"
+  end
+
+  scenario "with mismatching passwords" do 
+    fill_in "First Name", with: "Amy"
+    fill_in "Email", with: "amy@amazingamy.com"
+    fill_in "Password", with: "iamnotreallyamy"
+    fill_in "Password Confirmation", with: "iamreallyamy"
+    fill_in "Daily hours", with: 5 
+    click_button "Create User"
+    expect(page).to have_content "prohibited this user from being saved"
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+
+  scenario "with invalid work hours" do 
+    fill_in "First Name", with: "Amy"
+    fill_in "Email", with: "amy@amazingamy.com"
+    fill_in "Password", with: "iamnotreallyamy"
+    fill_in "Password Confirmation", with: "iamnotreallyamy"
+    fill_in "Daily hours", with: -5 
+    click_button "Create User"
+    expect(page).to have_content "prohibited this user from being saved"
+    expect(page).to have_content "Available daily work hours need to be greater than 0 and less than or equal to 24"
   end
 end
 
